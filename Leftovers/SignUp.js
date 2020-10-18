76fe789fcbd5e29d7e1b6865d33a51786758d38b
anasta098 <63991831+anasta098@users.noreply.github.com>
2020-10-18 23:38:13 +1100

    Website and Some app

--- Leftovers/SignUp.js
+++ Leftovers/SignUp.js
@@ -1,10 +1,13 @@
 function check(form)/*function to check userid & password*/
 {
  /*the following code checkes whether the entered userid and password are matching*/
- if(form.ConPswrd.value == "1234" && form.pswrd.value == "1234")
+  var password = document.getElementById("pswd").value;
+        var confirmPassword = document.getElementById("ConPswrd").value;
+ if(form.ConPswrd pswrd.value == "1234")
   {
     window.open('home.html')/*opens the target page while Id & password matches*/
-  }
+  } 
+  
  else
  {
    alert("Error Password or Username")/*displays error message*/ 
